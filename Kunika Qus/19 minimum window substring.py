3ea059e77b53c42576c5924f9562bb2d44a938fe
gitcoder-amit <yadavamit14801@gmail.com>
2026-03-30 07:36:43 +0530

    solved seach insert position

--- Kunika Qus/19 minimum window substring.py
+++ Kunika Qus/19 minimum window substring.py
@@ -31,79 +31,58 @@ class Solution:
         #your code goes here
         n = len(s)
         m = len(t)
-
-        sindex = -1
-        min_len = 100000000
+        res = ''
+        min_len = 1000000000
+        
         for i in range(n):
+            st = ''
             c = 0
-            # h = {}
             h = [0]*256
-            for k in t:
-                # h[k] = h.get(i, 0) + 1
-                h[ord(k)] += 1
-            # print(h)
+            for ch in t:
+                h[ord(ch)] += 1
             for j in range(i, n):
-                if h[ord(s[j])] != 0:
+                st += s[j]
+                if h[ord(s[j])] > 0:
                     c += 1
                     h[ord(s[j])] -= 1
-                    # if h[s[j]] == 0:
-                    #     del h[s[j]]
-
-            
+                
                 if c == m:
                     if j-i+1 < min_len:
                         min_len = j-i+1
-                        sindex = i
-        return s[sindex: sindex + min_len]
+                        res = st
+
+        return res
 
 
     def minWindow(self, s: str, t: str) -> str:
         # Time Complexity: O(n + m)
         # Space Complexity: O(1) for the hash map
-        
-        """ Variable to store the minimum 
-        length of substring found """
-        minLen = float('inf')
-        
-        """ Variable to store the starting index
-        of the minimum length substring """
-        sIndex = -1
-        
-        """ Array to count frequencies
-        of characters in string t"""
-        hash = [0] * 256
-        
-        # Count the frequencies of characters in t
-        for c in t:
-            hash[ord(c)] += 1
-            
-        count = 0
+        n = len(s)
+        m = len(t)
+        h = [0]*256
         l, r = 0, 0
-        
-        # Iterate through current window 
-        while r < len(s):
-            # Include the current character in the window
-            if hash[ord(s[r])] > 0:
-                count += 1
-            hash[ord(s[r])] -= 1
-                
-            """ If all characters from t 
-            are found in current window """
-            while count == len(t):
-                    
-                """ Update minLen and sIndex
-                if current window is smaller """
-                if r - l + 1 < minLen:
-                    minLen = r - l + 1
-                    sIndex = l
-                
-                # Remove leftmost character from window
-                hash[ord(s[l])] += 1
-                if hash[ord(s[l])] > 0:
-                    count -= 1
+        res = ''
+        min_len = 1000000
+
+        for ch in t:
+            h[ord(ch)] += 1
+        c = 0
+        while r < n:
+            if h[ord(s[r])] > 0:
+                c += 1
+            h[ord(s[r])] -= 1
+
+            while c == m:
+                if r-l+1 < min_len:
+                    min_len = r-l+1
+                    res = s[l:r+1]
+
+                h[ord(s[l])] += 1
+                if h[ord(s[l])] > 0:
+                    c -= 1
                 l += 1
+            
             r += 1
-        
-        # Return minimum length substring from s
-        return s[sIndex:sIndex + minLen] if sIndex != -1 else ""
+
+        return res
 
