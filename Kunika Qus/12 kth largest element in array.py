8e9fa91bec9dd4c699708b5ba6de146e40bf4175
gitcoder-amit <yadavamit14801@gmail.com>
2026-04-08 07:11:24 +0530

    Solved Kth largest element problem

--- Kunika Qus/12 kth largest element in array.py
+++ Kunika Qus/12 kth largest element in array.py
@@ -48,4 +48,20 @@ class Solution:
             k -= 1
         
         return -top
+
+
+        # using a min heap
+        # Time Complexity: O(n log k)
+        import heapq
+        pq = []
+
+        for i in range(k):
+            heapq.heappush(pq, nums[i])
+
+        for i in range(k, len(nums)):
+            if nums[i] > pq[0]:
+                heapq.heappop(pq)
+                heapq.heappush(pq, nums[i])
+        
+        return pq[0]
         
